135aebfaae99adfd90654c4d1eae3d285d0805dd
GnG <GnG@DESKTOP-LP7BFG7>
2022-09-12 05:32:51 +0900

    59P 완료

---
+++ TDD/src/main/Account.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Account {
+
+	public Account(int i) {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- TDD/src/test/AccountTest.java
+++ TDD/src/test/AccountTest.java
@@ -1,5 +1,36 @@
 package test;
 
-public class AccountTest {
+import static org.junit.Assert.fail;
 
+import org.junit.Test;
+
+import main.Account;
+
+public class AccountTest {
+	
+	@Test
+	public void testAccount() throws Exception {
+		Account account = new Account(10000);
+	}
+	
+	@Test
+	public void testGetBalance() throws Exception {
+		Account account = new Account(10000);
+		
+		if(account.getBalance() != 10000) {
+			fail();
+		}
+	}
+	
+	public static void main(String[] args) {
+		AccountTest test = new AccountTest();
+		try {
+			test.testAccount();	//계좌 생성
+			test.testGetBalance();	//잔고 조회 테스트
+		} catch (Exception e) {
+			System.out.println("실패(X)");
+			return;
+		}
+		System.out.println("성공(O)");
+	}
 }
