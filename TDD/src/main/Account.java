f9a120ee4c1c639eebbb0730f837c200fa75ef6d
GnG <GnG@DESKTOP-LP7BFG7>
2022-09-14 00:08:43 +0900

    88P 완료

--- TDD/src/main/Account.java
+++ TDD/src/main/Account.java
@@ -2,8 +2,25 @@ package main;
 
 public class Account {
 
-	public Account(int i) {
+	private int balance = 0;
+	public Account(int money) {
 		// TODO Auto-generated constructor stub
+		this.balance = money;
+	}
+
+	public int getBalance() {
+		// TODO Auto-generated method stub
+		return this.balance;
+	}
+
+	public void deposit(int money) {
+		// TODO Auto-generated method stub
+		this.balance += money;
+	}
+
+	public void withdraw(int money) {
+		// TODO Auto-generated method stub
+		this.balance -= money;
 	}
 	
 }
--- TDD/src/test/AccountTest.java
+++ TDD/src/test/AccountTest.java
@@ -1,32 +1,72 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import main.Account;
 
 public class AccountTest {
 	
-	@Test
-	public void testAccount() throws Exception {
-		Account account = new Account(10000);
+	private Account account;
+
+	@Before
+	public void setup() {
+		account = new Account(10000);
 	}
 	
+	//생성자 테스트
+	@Test
+	public void testAccount() throws Exception {
+	}
+	
+	//잔고 조회에 대한 테스트
 	@Test
 	public void testGetBalance() throws Exception {
-		Account account = new Account(10000);
+		//assertEquals("10000원으로 계좌 생성 후 잔고 조회", 10000, account.getBalance());
+		assertEquals(10000, account.getBalance());
+//		if(account.getBalance() != 10000) {
+//			fail("getBalance() => "+account.getBalance());
+//		}
 		
-		if(account.getBalance() != 10000) {
-			fail();
-		}
+		account = new Account(1000);
+		assertEquals(1000, account.getBalance());
+//		if(account.getBalance() != 1000) {
+//			fail();
+//		}
+		
+		account = new Account(0);
+		assertEquals(0, account.getBalance());
+//		if(account.getBalance() != 0) {
+//			fail();
+//		}
 	}
+
+	//입금에 대한 테스트
+	@Test
+	public void testDeposit() throws Exception{
+		account.deposit(1000);
+		assertEquals(11000, account.getBalance());
+	}
+	
+	//출금에 대한 테스트
+	@Test
+	public void testWithdraw() throws Exception{
+		account.withdraw(1000);
+		assertEquals(9000, account.getBalance());
+	}
+	
+	//출금에 대한 테스트
 	
 	public static void main(String[] args) {
 		AccountTest test = new AccountTest();
 		try {
 			test.testAccount();	//계좌 생성
 			test.testGetBalance();	//잔고 조회 테스트
+			test.testDeposit();	//입금 테스트
+			test.testWithdraw();	//출금 테스트
 		} catch (Exception e) {
 			System.out.println("실패(X)");
 			return;
