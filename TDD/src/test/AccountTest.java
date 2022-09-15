f6ec82ccfd79de6f4e9805f5ba11d9959a662f38
pring2580 <pring2580@gmail.com>
2022-09-15 23:24:35 +0900

    123P 완료

--- TDD/src/test/AccountTest.java
+++ TDD/src/test/AccountTest.java
@@ -1,6 +1,13 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
@@ -58,7 +65,37 @@ public class AccountTest {
 		assertEquals(9000, account.getBalance());
 	}
 	
-	//출금에 대한 테스트
+	//assert 테스트
+	@Test
+	public void testAssert() throws Exception{
+		//값이 같은지 비교
+		int a = 10;
+		int b = 10;
+		assertEquals("a != b", a, b);
+		
+		b = 5;
+		assertNotEquals("a == b", a, b);
+		
+		//객체가 같은지 비교
+		assertNotSame("객체 같음", a, b);
+		
+		a = b;
+		assertSame("객체 다름", a, b);
+		
+		//Null or Not Null
+		String s1 = null;
+		String s2 = "11";
+		assertNull("Not NULL", s1);
+		assertNotNull("NUll", s2);
+		
+		//True or False
+		a = 1;
+		b = 1;
+		assertTrue(a == b);
+		
+		b = 2;
+		assertFalse(a == b);
+	}
 	
 	public static void main(String[] args) {
 		AccountTest test = new AccountTest();
@@ -67,6 +104,9 @@ public class AccountTest {
 			test.testGetBalance();	//잔고 조회 테스트
 			test.testDeposit();	//입금 테스트
 			test.testWithdraw();	//출금 테스트
+			test.testAssert();	//assert 테스트
+			
+			
 		} catch (Exception e) {
 			System.out.println("실패(X)");
 			return;
